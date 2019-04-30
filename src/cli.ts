2c90ac4839544faf409fdea44ddb6aad17fa7fd3
Dave Gramlich <callmehiphop@gmail.com>
2019-04-30 15:47:48 -0400

    fixy

--- src/cli.ts
+++ src/cli.ts
@@ -19,65 +19,70 @@ import * as childProcess from 'child_process';
 import * as rcloadenv from '.';
 import * as yargs from 'yargs';
 
-const USAGE =
-    `Wrap execution of the given command with runtime-config variables.
+const USAGE = `Wrap execution of the given command with runtime-config variables.
 Usage: rcloadenv <configName> [options] -- [args...]`;
 
-const cli = exports.cli =
-    yargs.demand(1)
-        .usage(USAGE)
-        .options({
-          debug: {
-            alias: 'd',
-            description: 'Print debugging information.',
-            type: 'boolean'
-          },
-          except: {
-            alias: 'E',
-            description:
-                'If provided, a list of of runtime-config variables to exclude, otherwise all runtime-config variables will be used.',
-            type: 'array'
-          },
-          only: {
-            alias: 'O',
-            description:
-                'If provided, a list of of runtime-config variables to load, otherwise all runtime-config variables will be used.',
-            type: 'array'
-          },
-          override: {
-            alias: 'o',
-            default: false,
-            description:
-                'Determines the behavior of rcloadenv in the case when a runtime-config variable conflicts with a variable already available in the environment. If true, a runtime-config variable will override the existing environment variable, otherwise the existing environment variable will be kept.',
-            type: 'boolean'
-          },
-          projectId: {
-            alias: ['project', 'p'],
-            description: 'The project where the specified config is located.',
-            type: 'string'
-          }
-        })
-        .example(
-            `rcloadenv my-config -- bash -c 'echo $MY_VARIABLE_NAME'`,
-            `Print a variable's value from the specified config.`)
-        .example(
-            `rcloadenv my-config --only var1 var2 -- node app.js`,
-            `Only load two variables from runtime-config.`)
-        .example(
-            `rcloadenv my-config --except var1 var2 -- node app.js`,
-            `Load all but two variables from runtime-config.`)
-        .example(
-            `rcloadenv my-config --debug -- node app.js`,
-            `Print debugging information while loading variables.`)
-        .example(
-            `rcloadenv my-config --projectId my-project-id -- node app.js`,
-            `Specify the project ID to use when loading variables.`)
-        .wrap(120)
-        .recommendCommands()
-        .epilogue(
-            `For more information, see https://github.com/googleapis/nodejs-rcloadenv`)
-        .help()
-        .strict();
+const cli = (exports.cli = yargs
+  .demand(1)
+  .usage(USAGE)
+  .options({
+    debug: {
+      alias: 'd',
+      description: 'Print debugging information.',
+      type: 'boolean',
+    },
+    except: {
+      alias: 'E',
+      description:
+        'If provided, a list of of runtime-config variables to exclude, otherwise all runtime-config variables will be used.',
+      type: 'array',
+    },
+    only: {
+      alias: 'O',
+      description:
+        'If provided, a list of of runtime-config variables to load, otherwise all runtime-config variables will be used.',
+      type: 'array',
+    },
+    override: {
+      alias: 'o',
+      default: false,
+      description:
+        'Determines the behavior of rcloadenv in the case when a runtime-config variable conflicts with a variable already available in the environment. If true, a runtime-config variable will override the existing environment variable, otherwise the existing environment variable will be kept.',
+      type: 'boolean',
+    },
+    projectId: {
+      alias: ['project', 'p'],
+      description: 'The project where the specified config is located.',
+      type: 'string',
+    },
+  })
+  .example(
+    `rcloadenv my-config -- bash -c 'echo $MY_VARIABLE_NAME'`,
+    `Print a variable's value from the specified config.`
+  )
+  .example(
+    `rcloadenv my-config --only var1 var2 -- node app.js`,
+    `Only load two variables from runtime-config.`
+  )
+  .example(
+    `rcloadenv my-config --except var1 var2 -- node app.js`,
+    `Load all but two variables from runtime-config.`
+  )
+  .example(
+    `rcloadenv my-config --debug -- node app.js`,
+    `Print debugging information while loading variables.`
+  )
+  .example(
+    `rcloadenv my-config --projectId my-project-id -- node app.js`,
+    `Specify the project ID to use when loading variables.`
+  )
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(
+    `For more information, see https://github.com/googleapis/nodejs-rcloadenv`
+  )
+  .help()
+  .strict());
 
 async function main(argv: string[]) {
   const opts = cli.parse(argv);
@@ -91,17 +96,21 @@ async function main(argv: string[]) {
 
   const currentEnv = Object.assign({}, process.env);
   const env = await rcloadenv.getAndApply(
-      configName, currentEnv, opts as rcloadenv.RCLoadEnvOptions);
+    configName,
+    currentEnv,
+    opts as rcloadenv.RCLoadEnvOptions
+  );
   await new Promise((resolve, reject) => {
-    childProcess.spawn(args.shift()!, args, {stdio: 'inherit', env})
-        .on('error', reject)
-        .on('exit', (code) => {
-          if (code === 0) {
-            resolve();
-          } else {
-            reject(new Error('Command failed'));
-          }
-        });
+    childProcess
+      .spawn(args.shift()!, args, {stdio: 'inherit', env})
+      .on('error', reject)
+      .on('exit', code => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(new Error('Command failed'));
+        }
+      });
   });
 }
 
