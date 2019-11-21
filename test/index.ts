00b4a5265b6a673fba8d7b9df80f5594b2acef9a
Justin Beckwith <justin.beckwith@gmail.com>
2019-11-21 11:58:39 -0800

    fix(docs): release level should be beta

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016, Google, Inc.
+ * Copyright 2016 Google LLC
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,7 +14,7 @@
  */
 
 import * as assert from 'assert';
-import {Gaxios, GaxiosOptions} from 'gaxios';
+import {GaxiosOptions} from 'gaxios';
 import * as nock from 'nock';
 import * as proxyquire from 'proxyquire';
 
